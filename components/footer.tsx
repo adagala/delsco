986f39e134be3a32b8463120c88fe30f996935aa
Henry Adagala <adagalahenry@gmail.com>
2021-10-05 18:17:04 +0300

    added: header and footer

---
+++ components/footer.tsx
@@ -0,0 +1,10 @@
+
+const Footer = () => {
+    return (
+        <footer className="flex justify-center items-center h-12 bg-red-900 text-white">
+            &copy; 2021 Delsco Enterprises. All Rights Reserved.
+        </footer>
+    )
+}
+
+export default Footer
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Layout from '../components/layout'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className="flex flex-col h-screen bg-gray-100">
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </div>
+  )
 }
 export default MyApp
