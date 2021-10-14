982c0258fa630d67ea431d81292c7d45736de4fc
Henry Adagala <adagalahenry@gmail.com>
2021-10-14 15:34:35 +0300

    update: list interface to have property children

--- models/List.ts
+++ models/List.ts
@@ -1,11 +1,12 @@
 export interface List {
-    title: any;
+    title?: any;
     header?: string;
     description?: string;
+    children?: List[];
 }
 
 export interface AuditList {
-    title: any,
-    header?: any
-    children?: AuditList[]
+    title: any;
+    header?: any;
+    children?: AuditList[];
 }
