34db11052439b97d4f7f77770db5ce3420c5e89b
Henry Adagala <adagalahenry@gmail.com>
2022-02-21 07:19:34 +0300

    add: custom error page

---
+++ pages/404.tsx
@@ -0,0 +1,26 @@
+import { NextPage } from 'next'
+import Layout from "../components/layout"
+
+const Custom404: NextPage = () => {
+    return (
+        <Layout title="Error 404">
+            <main className="h-full font-work-sans border-t border-solid" >
+                <div className="h-full flex items-center justify-center">
+                    <div className="px-4 text-6xl font-extrabold text-red-900">
+                        404
+                    </div>
+                    <div className="border-l px-4">
+                        <div className="text-5xl font-extrabold">
+                            Page not Found
+                        </div>
+                        <div className="text-lg mt-2 text-gray-700">
+                            Please check the URL in the address bar and try again
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </Layout>
+    )
+}
+
+export default Custom404
