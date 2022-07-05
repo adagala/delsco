6ed44004f5f4cc784697cd19d1d5560b1792599f
Henry <adagalahenry@gmail.com>
2022-07-05 16:43:18 +0300

    republish website

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,33 @@
 import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { AnimatePresence } from "framer-motion";
+import { useRouter } from "next/router";
 
-function MyApp() {
+// The handler to smoothly scroll to the element into view
+const handExitComplete = (): void => {
+  if (typeof window !== "undefined") {
+    const hashId = window.location.hash;
+
+    if (hashId) {
+      const element = document.querySelector(hashId);
+
+      if (element) {
+        element.scrollIntoView({
+          behavior: "smooth",
+          block: "start",
+          inline: "nearest",
+        });
+      }
+    }
+  }
+};
+
+function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
   return (
-    <div className="h-screen w-full flex items-center justify-center">
-      <div className="font-bold text-3xl text-delsco-500 uppercase text-center">
-        Website Currently Unavailable
-      </div>
-    </div>
+    <AnimatePresence exitBeforeEnter onExitComplete={handExitComplete}>
+      <Component {...pageProps} key={router.route} />
+    </AnimatePresence>
   );
 }
 export default MyApp;
